a54613f5807181813e7750586f65508c0154cdee
Jon Carlstedt Tønnessen <jon.tonnessen@nb.no>
2024-02-27 16:40:27 +0100

    Update geoparse_NLI.py
    Adapted the script to implement Streamlit uploading functionality

--- geoparse_NLI.py
+++ geoparse_NLI.py
@@ -6,43 +6,56 @@ Created on Mon Feb 26 14:34:16 2024
 @author: joncto
 """
 
-# Importere pakker
-import requests
+import streamlit as st
 import pandas as pd
+import requests
+from io import BytesIO
 
+# Setup Streamlit layout
+st.title('Gatenavn til geo-koordinat')
 
-# Definere datakilde og laste inn data fra Excel
-file_path = './gateadresser.xlsx'
-df = pd.read_excel(file_path, header=None, names=['gateadresse'])
+# File uploader widget
+uploaded_file = st.file_uploader("Choose an Excel file", type=['xlsx'])
 
-# Funksjon for henting av geokoordinater fra GeoNorges API
-def get_geocoordinates(adresse):
-    base_url = "https://ws.geonorge.no/adresser/v1/sok"  # base URL for Geonorges API
-    params = {
-        'sok': adresse,  # Bruk 'adresse' parameter her
-        'treffPerSide': 1,  # Antall treff per side (antar vi bare ønsker toppresultatet)
-        'asciiKompatibel': 'true' # Garanterer at dataene som returneres er ascii-kompatible
-    }
-    response = requests.get(base_url, params=params)
-    data = response.json()
-    
-    # Henter ut breddegrad (lat) and lengdegrad (lon)
-    try:
-        lat = data['adresser'][0]['representasjonspunkt']['lat']
-        lon = data['adresser'][0]['representasjonspunkt']['lon']
-        return lat, lon
-    except (IndexError, KeyError):
-        return None, None  # I tilfeller der adressen ikke blir funnet eller responsen ikke er som forventet
-    
-# print(df.columns)
+if uploaded_file is not None:
+    df = pd.read_excel(uploaded_file, header=None, names=['gateadresse'])
 
-# Legger til geokoordinater for hver gateadresse i 'latitude' og 'longitude'.
-df[['latitude', 'longitude']] = df.apply(lambda row: pd.Series(get_geocoordinates(row['gateadresse'])), axis=1)
+    def get_geocoordinates(adresse):
+        base_url = "https://ws.geonorge.no/adresser/v1/sok"
+        params = {
+            'sok': adresse,
+            'treffPerSide': 1,
+            'asciiKompatibel': 'true'
+        }
+        response = requests.get(base_url, params=params)
+        data = response.json()
 
-# Lagrer oppdatert dataframe til ny Excel-fil
-output_file_path = './output.xlsx'
-df.to_excel(output_file_path, index=False)
+        try:
+            lat = data['adresser'][0]['representasjonspunkt']['lat']
+            lon = data['adresser'][0]['representasjonspunkt']['lon']
+            return lat, lon
+        except (IndexError, KeyError):
+            return None, None
 
-# Lagrer oppdatert dataframe til ny CSV fil
-csv_output_file_path = './output.csv'
-df.to_csv(csv_output_file_path, index=False)
+    # Process data
+    df[['latitude', 'longitude']] = df.apply(lambda row: pd.Series(get_geocoordinates(row['gateadresse'])), axis=1)
+
+    # Convert DataFrame to Excel in memory
+    output = BytesIO()
+    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+        df.to_excel(writer, index=False)
+        writer.save()
+    output.seek(0)
+
+    # Download link
+    st.download_button(label="Download Excel",
+                       data=output,
+                       file_name="output.xlsx",
+                       mime="application/vnd.ms-excel")
+
+    # Display DataFrame in Streamlit (optional)
+    st.dataframe(df)
+
+# Instructions
+else:
+    st.write("Please upload an Excel file to begin.")
